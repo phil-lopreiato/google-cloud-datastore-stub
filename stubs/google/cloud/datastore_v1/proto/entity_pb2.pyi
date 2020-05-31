5a5d762c7cd64230751d2a427b51ed82c2b11036
Phil Lopreiato <plnyyanks@gmail.com>
2020-05-31 11:52:11 -0400

    add typecheck stubs for google libs

---
+++ stubs/google/cloud/datastore_v1/proto/entity_pb2.pyi
@@ -0,0 +1,8 @@
+from typing import Any
+
+DESCRIPTOR: Any
+PartitionId: Any
+Key: Any
+ArrayValue: Any
+Value: Any
+Entity: Any
---
+++ stubs/google/cloud/datastore_v1/proto/query_pb2.pyi
@@ -0,0 +1,15 @@
+from typing import Any
+
+DESCRIPTOR: Any
+EntityResult: Any
+Query: Any
+KindExpression: Any
+PropertyReference: Any
+Projection: Any
+PropertyOrder: Any
+Filter: Any
+CompositeFilter: Any
+PropertyFilter: Any
+GqlQuery: Any
+GqlQueryParameter: Any
+QueryResultBatch: Any
