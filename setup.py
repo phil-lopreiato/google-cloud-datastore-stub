8a89e24906aeabbaad069edecd10a5dc46fe160f
Phil Lopreiato <plnyyanks@gmail.com>
2020-06-27 23:16:28 -0400

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="InMemoryCloudDatastoreStub",
-    version="0.0.6",
+    version="0.0.7",
     description="An in-memory stub implementation of Google Cloud Datastore for use in unit tests",
     long_description=long_description,
     long_description_content_type="text/markdown",
