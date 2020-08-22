01c866cc703babdd2ad15006cbac70e385aac063
Zachary Orr <516458+ZachOrr@users.noreply.github.com>
2020-08-22 15:30:05 -0400

    Relax google-cloud-ndb version requirements
    We're getting errors when installing TBA dependencies because we have conflicting versions of `google-cloud-ndb` - imdb requires `1.2.1` and we install `1.5.0`
    
    This just relaxes the version requirement to be at least `1.2.1`, which should allow us to install things without getting errors

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     url="https://github.com/phil-lopreiato/google-cloud-datastore-stub/",
     packages=setuptools.find_packages(),
     python_requires=">=3",
-    install_requires=["google-cloud-ndb == 1.2.1"],
+    install_requires=["google-cloud-ndb >= 1.2.1"],
     setup_requires=["pytest-runner"],
     tests_require=["pytest", "black", "pyre-check", "flake8", "mypy"],
 )
